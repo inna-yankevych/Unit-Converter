e4ba0490a6533ea3c92babd1c109ff4d76a99dd2
Inna Yankevych <inna.yankevych@gmail.com>
2025-07-08 17:38:05 +1000

    planning units for conversion

---
+++ src/functions.py
@@ -0,0 +1,56 @@
+# Units I want to convert:
+
+# TEMPERATURE:
+# Celsius
+# Farenheit
+# Kelvin
+
+# LENGTH / DISTANCE:
+# millimeters
+# centimeters
+# meters
+# kilometers
+# inches
+# feet
+# yards
+# miles
+
+# WEIGHT / MASS:
+# milligrams
+# grams
+# kilograms
+# ounces
+# pounds
+# tons
+
+# VOLUME / CAPACITY:
+# milliliters
+# liters
+# cups
+# pints
+# quarts
+# gallons
+# fluid ounces
+
+# TIME:
+# seconds
+# minutes
+# hours
+# days
+# weeks
+# months
+# years
+
+# SPEED:
+# meters per second
+# kilometers per hour
+# miles per hour
+
+# AREA:
+# square meter
+# square kilometer
+# square mile
+# square yard
+# square foot
+# hectare
+# acre
